a1119a010761a85604cec9c9cff0a6de294c344e
Christoph D. Hermann, ITBH <christoph.hermann@itbh.at>
2018-11-21 10:21:04 +0100

    Add support for copy and paste

--- main.js
+++ main.js
@@ -3,6 +3,26 @@
 const electron = require('electron');
 const app = require('app');
 const BrowserWindow = require('browser-window');
+const Menu = require("menu");
+
+const template = [{
+    label: "Application",
+    submenu: [
+        { label: "About Application", selector: "orderFrontStandardAboutPanel:" },
+        { type: "separator" },
+        { label: "Quit", accelerator: "Command+Q", click: function() { app.quit(); }}
+    ]}, {
+    label: "Edit",
+    submenu: [
+        { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+        { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+        { type: "separator" },
+        { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+        { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+        { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+        { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+    ]}
+];
 
 let mainWindow;
 
@@ -10,6 +30,8 @@ function createWindow() {
     mainWindow = new BrowserWindow({width: 1024, height: 768});
     mainWindow.loadURL('file://' + __dirname + '/index.html');
     // mainWindow.webContents.openDevTools();
+
+    Menu.setApplicationMenu(Menu.buildFromTemplate(template));
 
     mainWindow.on('closed', function () {
         mainWindow = null;
